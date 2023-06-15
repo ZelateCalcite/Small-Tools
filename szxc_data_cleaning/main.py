61f8b89e8a85c32abd4401985aec9eae778cedd8
zhangzhihao <1105901242@qq.com>
2023-06-15 14:02:44 +0800

    封装完成

--- szxc_data_cleaning/main.py
+++ szxc_data_cleaning/main.py
@@ -2,6 +2,7 @@ from read_dir import ReadDir
 from excel_process import Operator
 from warnings import simplefilter
 from re import search, sub
+from custom_check import zzxx, hjrk, jtfw, czfjtfw, czffjtfw, xsm, qyxx, xxnyjyzt, cfxx, tdxx, dyxx, xmgl, mqrj, zlgl
 
 simplefilter('ignore')
 
@@ -22,26 +23,140 @@ file_sort = {
     '民情日记': 13,
     '租赁管理': 14
 }
+relations = {
+    '组织信息': None,
+    '户籍人口': ['组织信息'],
+    '家庭房屋': ['户籍人口'],
+    '出租房（家庭房屋）': ['家庭房屋'],
+    '出租房（非家庭房屋）': ['出租房（家庭房屋）'],
+    '新市民': ['出租房（家庭房屋）', '出租房（非家庭房屋）'],
+    '企业信息': None,
+    '新型农业经营主体': None,
+    '厂房信息': None,
+    '土地信息': ['户籍人口'],
+    '党员信息': None,
+    '项目管理': None,
+    '民情日记': ['组织信息', '户籍人口'],
+    '租赁管理': None
+}
+public_data = {}
 
 
-def check_value(file, data):
-    pass
+def check_relation_file(key: str, raw: dict, related: dict) -> (bool, str):
+    if related.get(key) is None:
+        return True, ''
+    else:
+        miss = []
+        valid = True
+        for i in related.get(key):
+            if raw.get(i) is None:
+                valid = False
+                miss.append(i)
+            else:
+                v, m = check_relation_file(i, raw, related)
+                valid &= v
+                if m != '':
+                    miss.append(m)
+        temp = ''
+        if miss:
+            temp = key + ': 缺失' + ', '.join(miss) + '.'
+        return valid, temp
+
+
+def data_reg(filepath: str) -> str:
+    fd = {
+        '组织信息': 1,
+        '户籍人口': 2,
+        '家庭房屋': 3,
+        '出租房（家庭房屋）': 4,
+        '出租房（非家庭房屋）': 5,
+        '新市民': 6,
+        '企业信息': 7,
+        '新型农业经营主体': 8,
+        '厂房信息': 9,
+        '土地信息': 10,
+        '党员信息': 11,
+        '项目管理': 12,
+        '民情日记': 13,
+        '租赁管理': 14
+    }
+    temp = search(r'[^/]*\.xlsx$', filepath)
+    filename = sub(r'\)', '）', sub(r'\(', '（', temp.string[temp.regs[0][0]:temp.regs[0][1]]))
+    filetype = ''
+    for key in fd.keys():
+        if search(key + '_', filename) is not None:
+            filetype = key
+            break
+    return filetype
+
+
+def check_value(filename, operator):
+    funcs = {
+        '组织信息': zzxx,
+        '户籍人口': hjrk,
+        '家庭房屋': jtfw,
+        '出租房（家庭房屋）': czfjtfw,
+        '出租房（非家庭房屋）': czffjtfw,
+        '新市民': xsm,
+        '企业信息': qyxx,
+        '新型农业经营主体': xxnyjyzt,
+        '厂房信息': cfxx,
+        '土地信息': tdxx,
+        '党员信息': dyxx,
+        '项目管理': xmgl,
+        '民情日记': mqrj,
+        '租赁管理': zlgl
+    }
+    fun = funcs[filename]
+    if filename == '组织信息':
+        rtn = fun(operator.import_data)
+        public_data.update({'小组': rtn['checked']['组织名称']})
+    elif filename == '户籍人口':
+        rtn = fun(operator.import_data, public_data['小组'])
+        public_data.update({
+            '门牌号': rtn['checked']['门牌号'],
+            '户主姓名': rtn['checked']['姓名'],
+            '身份证号': rtn['checked']['身份证号']
+        })
+    elif filename == '家庭房屋':
+        rtn = fun(operator.import_data, public_data['门牌号'])
+        public_data.update({
+            '家庭门牌号': rtn['checked']['门牌号'],
+            '家庭房屋编号': rtn['checked']['房屋编号']
+        })
+    elif filename == '出租房（家庭房屋）':
+        rtn = fun(operator.import_data, {'门牌号': public_data['家庭门牌号'], '房屋编号': public_data['家庭房屋编号']})
+        public_data.update({
+            '出租房家庭门牌号': rtn['checked']['门牌号'],
+            '出租房家庭房屋编号': rtn['checked']['房屋编号']
+        })
+    elif filename == '出租房（非家庭房屋）':
+        rtn = fun(operator.import_data, {'门牌号': public_data['出租房家庭门牌号'], '房屋编号': public_data['出租房家庭房屋编号']})
+        public_data.update({
+            '出租房非家庭门牌号': rtn['checked']['门牌号']
+        })
+    elif filename == '新市民':
+        temp = {}
+        temp.update(public_data['出租房家庭门牌号'])
+        temp.update(public_data['出租房非家庭门牌号'])
+        rtn = fun(operator.import_data, temp)
+    elif filename == '土地信息':
+        rtn = fun(operator.import_data, public_data['身份证号'])
+    elif filename == '民情日记':
+        rtn = fun(operator.import_data, {'小组': public_data['小组'], '身份证号': public_data['身份证号']})
+    else:
+        rtn = fun(operator.import_data)
+    return rtn['export']
 
 
 def main():
-    files = ReadDir('./tests/import', '.xlsx').file_list
+    files = ReadDir('./tests/import', '.xlsx', file_sort, data_reg).file_list
     all_data = {}
-    checked_data = {}
+    available = {}
 
     for index, filepath in enumerate(files):
         operator = Operator()
-        temp = search(r'[^/]*\.xlsx$', filepath)
-        filename = sub(r'\)', '）', sub(r'\(', '（', temp.string[temp.regs[0][0]:temp.regs[0][1]]))
-        filetype = ''
-        for key in file_sort.keys():
-            if search(key + '_', filename) is not None:
-                filetype = key
-                break
+        filetype = data_reg(filepath)
         if filetype != '':
             operator.export_data = operator.import_excel(filepath)
             if all_data.get(filetype) is None:
@@ -49,15 +164,20 @@ def main():
             else:
                 print('存在重复{0}模板'.format(filetype))
                 return 10002
-    if len(all_data) < len(file_sort):
-        print('缺失模板')
-        return 10001
+    print('---\t正在检查文件关联性\t---')
+    for key in all_data.keys():
+        v, m = check_relation_file(key, all_data, relations)
+        if not v:
+            print(m)
+        else:
+            available.update({key: all_data[key]})
+    if len(available) == len(all_data):
+        print('无关联文件缺失')
 
-    for filename, filedata in all_data.items():
+    for filename, filedata in available.items():
         try:
-            for title, cells in filedata.export_data.items():
-                check_value(title, cells)
-            filedata.export_excel(filename=filename.replace('import', 'export'))
+            filedata.export_data = check_value(filename, filedata)
+            filedata.export_excel(filename=filename+'_导出模板.xlsx')
         except BaseException as error:
             print(error)
 
--- szxc_data_cleaning/read_dir.py
+++ szxc_data_cleaning/read_dir.py
@@ -1,10 +1,26 @@
 from os import listdir
 from re import compile, sub, match
+from functools import cmp_to_key
 
 
 class ReadDir:
-    def __init__(self, path, suffix):
+    def __init__(self, path, suffix, sort_dict=None, sort_process=None):
+        self.sort_process = sort_process
+        self.sort_dict = sort_dict
         self.file_list = self.__read_files(path, suffix)
+
+    def __cmp(self, a, b):
+        if self.sort_process:
+            a, b = self.sort_process(a), self.sort_process(b)
+        if self.sort_dict.get(a):
+            a = self.sort_dict[a]
+        else:
+            a = float('inf')
+        if self.sort_dict.get(b):
+            b = self.sort_dict[b]
+        else:
+            b = float('inf')
+        return 1 if a > b else -1 if a < b else 0
 
     def __read_files(self, path: str, suffix: str) -> [str]:
         path = sub('/$', '', path)
@@ -15,6 +31,7 @@ class ReadDir:
         for file in files:
             if match(pattern, file) and self.__access_file('{0}/{1}'.format(path, file)):
                 result.append('{0}/{1}'.format(path, file))
+        result.sort(key=cmp_to_key(self.__cmp))
         return result
 
     @staticmethod
